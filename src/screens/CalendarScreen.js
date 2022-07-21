1f51e2f7085b41bdd0d4cf84e4b6f1a16cc785b3
createit-jungjunghoon <92363576+walkerJung@users.noreply.github.com>
2022-07-21 23:14:07 +0900

    # calendar body, prev button, next button 구현

--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -1,34 +1,100 @@
 import React, {useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, View, Dimensions} from 'react-native';
+import styled from 'styled-components/native';
+import {
+  getMonth,
+  startOfMonth,
+  getWeeksInMonth,
+  startOfWeek,
+  addDays,
+  getDate,
+  format,
+} from 'date-fns';
+
+const CalendarHeader = ({windowWidth}) => {
+  const week = ['일', '월', '화', '수', '목', '금', '토'];
+
+  return week.map(day => (
+    <Text
+      style={{
+        color: day === '토' ? 'blue' : day === '일' ? 'red' : 'black',
+        width: Math.floor(windowWidth / 7),
+        textAlign: 'center',
+      }}>
+      {day}
+    </Text>
+  ));
+};
+
+const CalendarBody = ({year, month, windowWidth}) => {
+  const _date = new Date(year, month - 1, 1);
+  const monthStart = startOfMonth(_date);
+  const weekLength = getWeeksInMonth(monthStart);
+  const weekStart = startOfWeek(monthStart, {weekStartsOn: 0});
+  const weekList = [];
+
+  for (let i = 0; i < weekLength * 7; i++) {
+    const tempDate = addDays(weekStart, i);
+    const today = format(new Date(), 'MM/dd/yyyy');
+
+    if (today === format(tempDate, 'MM/dd/yyyy')) {
+      weekList.push({
+        isToday: true,
+        day: getDate(tempDate),
+      });
+    } else if (getMonth(tempDate) + 1 === month) {
+      weekList.push({
+        isPrevOrNext: false,
+        day: getDate(tempDate),
+      });
+    } else {
+      weekList.push({
+        isPrevOrNext: true,
+        day: getDate(tempDate),
+      });
+    }
+  }
+
+  return weekList.map((item, idx) => (
+    <Text
+      key={idx}
+      style={{
+        width: Math.floor(windowWidth / 7),
+        textAlign: 'center',
+        borderWidth: item.isToday && 1,
+        borderRadius: item.isToday && 10,
+        borderStyle: item.isToday && 'solid',
+        borderColor: item.isToday && 'purple',
+        opacity: item.isPrevOrNext ? 0.3 : 1,
+      }}
+      onPress={() => console.log('@TODO - click event')}>
+      {item.day}
+    </Text>
+  ));
+};
 
 const CalendarScreen = () => {
-  const week = ['일', '월', '화', '수', '목', '금', '토'];
-  const today = {
-    year: new Date().getFullYear(),
-    month: new Date().getMonth() + 1,
-    date: new Date().getDate(),
-    day: new Date().getDay(),
-  };
-  const [selectedYear, setSelectedYear] = useState(today.year);
-  const [selectedMonth, setSelectedMonth] = useState(today.month);
-  const yearMonth = selectedYear + '년' + ' ' + selectedMonth + '월';
-  const dateTotalCount = new Date(selectedYear, selectedMonth, 0).getDate();
+  const [date, setDate] = useState(new Date());
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [month, setMonth] = useState(new Date().getMonth() + 1);
+  const yearMonth = year + '년' + ' ' + month + '월';
+  const windowWidth = Dimensions.get('window').width;
 
   const prevMonth = () => {
-    if (selectedMonth === 1) {
-      setSelectedMonth(12);
-      setSelectedYear(selectedYear - 1);
+    if (month === 1) {
+      setMonth(12);
+      setYear(year - 1);
     } else {
-      setSelectedMonth(selectedMonth - 1);
+      setMonth(month - 1);
     }
   };
 
   const nextMonth = () => {
-    if (selectedMonth === 12) {
-      setSelectedMonth(1);
-      setSelectedYear(selectedYear + 1);
+    if (month === 12) {
+      setMonth(1);
+      setYear(year + 1);
     } else {
-      setSelectedMonth(selectedMonth + 1);
+      setMonth(month + 1);
     }
   };
 
@@ -43,6 +109,16 @@ const CalendarScreen = () => {
         <Button title="<" onPress={prevMonth} />
         <Text>{yearMonth}</Text>
         <Button title=">" onPress={nextMonth} />
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <CalendarHeader windowWidth={windowWidth} />
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }}>
+        <CalendarBody year={year} month={month} windowWidth={windowWidth} />
       </View>
     </View>
   );
